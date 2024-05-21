d92654b29218cf190244d54f4d0e50d08ed4ea77
Andrew Kvalheim <andrew@kvalhe.im>
2024-05-21 08:54:54 -0700

    Use rstest

--- tests/worlds.rs
+++ tests/worlds.rs
@@ -1,14 +1,14 @@
-use forgiving_semver::Version;
 use image::{GenericImageView, Pixel};
 use itertools::{assert_equal, Itertools};
 use little_a_map::{level::Level, palette, render, search};
-use once_cell::sync::Lazy;
+use rstest::*;
+use rstest_reuse::{self, *};
 use serde::Deserialize;
 use std::collections::HashSet;
 use std::fs::File;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
+use std::str::FromStr;
 use tempfile::TempDir;
-use test_context::{test_context, TestContext};
 
 const MAP_IDS: [u32; 12] = [
     0, // Player inventory
@@ -52,65 +52,43 @@ struct World {
     pub output: TempDir,
 }
 
-impl World {
-    fn load(major: u64, minor: u64, patch: u64) -> Self {
-        let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-            .join(format!("fixtures/world-{major}.{minor}.{patch}"));
+impl FromStr for World {
+    type Err = ();
 
-        let level = Level::from_world_path(&path).unwrap();
+    fn from_str(version: &str) -> Result<Self, Self::Err> {
+        let input =
+            PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(format!("fixtures/world-{version}"));
         let output = tempfile::tempdir_in(env!("TEST_OUTPUT_PATH")).unwrap();
-        let output_path = output.path();
 
-        assert_eq!(level.version, Version::new(major, minor, patch));
-        let ids = search(&path, output_path, true, true, None).unwrap();
-        render(&path, output_path, true, true, &level, &ids).unwrap();
+        let level = Level::from_world_path(&input).unwrap();
+        assert_eq!(level.version.to_string(), version);
 
-        Self { ids, level, output }
+        let ids = search(&input, output.path(), true, true, None).unwrap();
+        render(&input, output.path(), true, true, &level, &ids).unwrap();
+
+        Ok(Self { ids, level, output })
     }
 }
 
-struct Worlds([Lazy<World>; 3]);
+#[template]
+#[rstest]
+#[case::world_1_20_2("1.20.2")]
+#[case::world_1_20_4("1.20.4")]
+#[case::world_1_20_6("1.20.6")]
+fn worlds(#[case] world: World) {}
 
-impl TestContext for Worlds {
-    fn setup() -> Self {
-        Self([
-            Lazy::new(|| World::load(1, 20, 2)),
-            Lazy::new(|| World::load(1, 20, 4)),
-            Lazy::new(|| World::load(1, 20, 6)),
-        ])
-    }
+#[apply(worlds)]
+fn spawn(world: World) {
+    assert_eq!((world.level.spawn_x, world.level.spawn_z), (0, 0));
 }
 
-fn assert_swatch(path: impl AsRef<Path>) {
-    let view = image::open(path).unwrap();
-
-    assert_eq!(view.dimensions(), (128, 128));
-
-    for (i, rgb) in (0..).zip(palette::BASE.into_iter()).skip(1) {
-        let pixel = view.get_pixel(i, 0);
-        assert_eq!(pixel.to_rgb(), rgb.into());
-    }
+#[apply(worlds)]
+fn map_ids(world: World) {
+    assert_equal(world.ids.iter().sorted(), &MAP_IDS);
 }
 
-#[test_context(Worlds)]
-#[test]
-fn spawn(worlds: &mut Worlds) {
-    for world in &worlds.0 {
-        assert_eq!((world.level.spawn_x, world.level.spawn_z), (0, 0));
-    }
-}
-
-#[test_context(Worlds)]
-#[test]
-fn map_ids(worlds: &mut Worlds) {
-    for world in &worlds.0 {
-        assert_equal(world.ids.iter().sorted(), &MAP_IDS);
-    }
-}
-
-#[test_context(Worlds)]
-#[test]
-fn banners(worlds: &mut Worlds) {
+#[apply(worlds)]
+fn banners(world: World) {
     #[derive(Deserialize)]
     struct GeoJson {
         features: Vec<Feature>,
@@ -128,28 +106,22 @@ fn banners(worlds: &mut Worlds) {
         pub color: String,
     }
 
-    for world in &worlds.0 {
-        let json = File::open(world.output.path().join("banners.json")).unwrap();
-        let geo: GeoJson = serde_json::from_reader(json).unwrap();
+    let json = File::open(world.output.path().join("banners.json")).unwrap();
+    let geo: GeoJson = serde_json::from_reader(json).unwrap();
 
-        let actual = geo.features.into_iter().sorted().map(|f| (f.name, f.color));
-        let expected = BANNERS.iter().map(|&(n, c)| (n.map(Into::into), c.into()));
-        assert_equal(actual, expected);
-    }
+    let actual = geo.features.into_iter().sorted().map(|f| (f.name, f.color));
+    let expected = BANNERS.iter().map(|&(n, c)| (n.map(Into::into), c.into()));
+    assert_equal(actual, expected);
 }
 
-#[test_context(Worlds)]
-#[test]
-fn map_swatch(worlds: &mut Worlds) {
-    for world in &worlds.0 {
-        assert_swatch(world.output.path().join("maps/1.png"));
-    }
-}
+#[apply(worlds)]
+fn swatch(world: World, #[values("maps/1.png", "tiles/4/0/0.png")] path: &str) {
+    let view = image::open(world.output.path().join(path)).unwrap();
 
-#[test_context(Worlds)]
-#[test]
-fn tile_swatch(worlds: &mut Worlds) {
-    for world in &worlds.0 {
-        assert_swatch(world.output.path().join("tiles/4/0/0.png"));
+    assert_eq!(view.dimensions(), (128, 128));
+
+    for (i, rgb) in (0..).zip(palette::BASE.into_iter()).skip(1) {
+        let pixel = view.get_pixel(i, 0);
+        assert_eq!(pixel.to_rgb(), rgb.into());
     }
 }
