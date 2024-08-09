2f8436e857da676bdef3a78a689d15a27f3d9595
Andrew Kvalheim <andrew@kvalhe.im>
2024-08-09 10:32:28 -0700

    Test against Minecraft 1.21.1

--- tests/worlds.rs
+++ tests/worlds.rs
@@ -124,6 +124,7 @@ fn observe_modifications(base: &Path) -> HashMap<String, SystemTime> {
 #[case::world_1_20_4("1.20.4")]
 #[case::world_1_20_6("1.20.6")]
 #[case::world_1_21_0("1.21.0")]
+#[case::world_1_21_0("1.21.1")]
 fn worlds(#[case] world: World) {}
 
 #[apply(worlds)]
